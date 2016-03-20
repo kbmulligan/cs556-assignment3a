9f704b5129e4436f37ca67d5d4ccad1d80b6ea7d
kbmulligan <brett.mulligan@gmail.com>
2016-03-20 10:49:04 -0700

    Refactored for more C++ style.

--- encrypt.cpp
+++ encrypt.cpp
@@ -1,38 +1,38 @@
 // encrypt.c - demonstrates a vigenere cipher and a transposition cipher 
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <assert.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cassert>
 
+using namespace std;
 
 const bool CL_ARGS = false;
 
-
 const int ALPHABET_LENGTH = 26;
 
-const char ENCRYPTED_FILENAME[] = "kevin-mulligan-encrypted-str";
-const char DECRYPTED_FILENAME[] = "kevin-mulligan-decrypted-str";
+string ENCRYPTED_FILENAME("kevin-mulligan-encrypted-str");
+string DECRYPTED_FILENAME("kevin-mulligan-decrypted-str");
 
-char fileToEncrypt[] = "default-file";
+string fileToEncrypt("default-file");
 
 const int KEY_LENGTH = 10;
-char key1[] = "KEYONEKEYO";
-char key2[] = "KEYTWOKEYT";
+string key1 = "KEYONEKEYO";
+string key2 = "KEYTWOKEYT";
 
 
-
+// FUNCTION SIGNATURES //////////////////////////
 char encryptChar (char m, char k);
 
-FILE* openFileForRead (const char* filename);
-FILE* openFileForWrite (const char* filename);
-int closeFile (FILE* pFile);
+int openFileForRead (string filename);
+int closeFile (fstream);
 
-const char* getKey (char * keyBuffer);
+string getKey (void);
 int processArgs (int count, char* argv[]);
 
-// HELPER FUNCTIONS /////////////////////////
 
+
+// HELPER FUNCTIONS /////////////////////////////
 char encryptChar (char m, char k) {
     char c = 0;
     
@@ -42,47 +42,22 @@ char encryptChar (char m, char k) {
 }
 
 
-FILE* openFileForRead (const char* filename) {
-    FILE* pFile = NULL;
-
-    pFile = fopen(filename, "rb");
-    if (pFile != NULL) {
-        puts("File opened for read...");
-
-
-
-
-    }
-    return pFile;
+int openFileForRead (string filename) {
+    return 0;
 }
 
-
-FILE* openFileForWrite (const char* filename) {
-    FILE* pFile = NULL;
-
-    pFile = fopen(filename, "wb");
-    if (pFile != NULL) {
-        puts("File opened for write...");
-
-
-
-
-        // fclose(pFile);
-    }
-    return pFile;
-}
-
-int closeFile (FILE* pFile) {
-    fclose(pFile);
+int closeFile (fstream file) {
+    file.close();
     return 0;
 }
 
 
-const char* getKey(char* keyBuffer) {
-    puts("Enter key: ");
-    printf("keyBuffer length before input is: %d\n", (int)sizeof(keyBuffer));
-    fgets(keyBuffer, sizeof(keyBuffer), stdin);
-    printf("keyBuffer length after input is: %d\n", (int)sizeof(keyBuffer));
+string getKey() {
+    cout << "Enter key: " << endl;
+    string keyBuffer("PLACEHOLDER STRING HERE");
+    cin >> keyBuffer;
+    cout << "keyBuffer now containts:" << keyBuffer << endl;
+
     return keyBuffer;
 }
  
@@ -91,39 +66,56 @@ int processArgs(int count, char* argv[]) {
     return 0;
 }
 
+int printFile(FILE* file) {
+
+    return 0;
+}
+
 int main (int argc, char* argv[]) {
     printf("Encrypt..................\n");
 
-    FILE* fileInput = NULL;
-    FILE* fileOutput = NULL;
+    ifstream fileInput;
 
 
     // get keys and filename
     if (CL_ARGS) {
         processArgs(argc, argv);    
     } else {
-        char keyBuffer[64];
-        
-        printf("sizeof keyBuffer is : %d\n", (int)sizeof(keyBuffer));
-
-        strncpy(key1, getKey(keyBuffer), strlen(key1));
-        strncpy(key2, getKey(keyBuffer), strlen(key2));
+        key1 = getKey();
+        key2 = getKey();
     }
 
-    printf("key1 length is : %d\n", (int)strlen(key1));
-    printf("key2 length is : %d\n", (int)strlen(key2));
-    puts(key1);
-    puts(key2);
+    printf("key1 length is : %d\n", (int)key1.length());
+    printf("key2 length is : %d\n", (int)key2.length());
+    cout << "Key1 is: " << key1 << endl;
+    cout << "Key2 is: " << key2 << endl;
 
-    assert(strlen(key1) == KEY_LENGTH);
-    assert(strlen(key2) == KEY_LENGTH);
-
-    fileInput = openFileForRead(fileToEncrypt);
-    closeFile(fileInput);
-
-    fileOutput = openFileForWrite(ENCRYPTED_FILENAME);
-    closeFile(fileOutput);
+    assert(key1.length() == KEY_LENGTH);
+    assert(key1.length() == KEY_LENGTH);
+    cout << "GOOD KEYS!" << endl;
+    
 
 
-    exit(0);
+
+    ifstream inputFile (fileToEncrypt.c_str(), ios::in | ios::binary);
+
+    if (inputFile.is_open()) {
+        cout << "File opened for read..." << endl;
+        cout << "CONTENTS OF INPUT FILE:" << endl;
+        string line;
+        while (getline(inputFile, line)) {
+            cout << line << endl;
+        }
+
+    } else {
+        cout << "Failed to open file!!!!!!" << endl;
+    }
+
+
+
+
+
+
+
+    return 0;
 }
